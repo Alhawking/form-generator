15dd81f41b6d464981f32d7e1fdbb5a2a95d5d46
Alfredo Ledesma <76226210+Alhawking@users.noreply.github.com>
2021-08-19 10:56:55 -0500

    Changes in the input to search options.
    The input to search has some improvements and the window to search is showed while inserting options with a snippet

--- CustomSelect.js
+++ CustomSelect.js
@@ -8,9 +8,32 @@ export default class CustomSelect{
         this.endPoint = endPoint;
         this.options = [];
         this.button;
-        this.option;
     }
-    selectOption(options,button) {
+    deleteDiacritic(texto) {
+        return texto
+            .normalize('NFD')
+            .replace(/([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,"$1")
+            .normalize();
+    }
+    selectOption(button,divOption){
+        const deleteButton = UI.createElement('div');
+        deleteButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-square-x" width="48" height="48" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <rect x="4" y="4" width="16" height="16" rx="2" />
+        <path d="M10 10l4 4m0 -4l-4 4" />
+        </svg>`
+        divOption.appendChild(deleteButton);
+        this.replace(button.parentNode,divOption,button);
+        document.querySelector('.div-search').remove();
+        deleteButton.addEventListener('click',()=>{
+            this.replace(divOption.parentNode,button,divOption);
+        },true);
+    }
+    createOptions(options,button) {
+        const spinner =document.querySelector('#spinner')
+        if(spinner){
+            spinner.remove();
+        }
             options.forEach(option=>{
                 const divOption = UI.createElement('div',{},['custom-option']);
                 divOption.dataset.name = this.name;
@@ -18,23 +41,31 @@ export default class CustomSelect{
                 <span>${option.label}</span>
                 `
                 divOption.dataset.value = option.value;
+                const obj = this;
                 divOption.addEventListener('click',function handler(){
-                    const deleteButton = UI.createElement('div');
-                    deleteButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-square-x" width="48" height="48" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <rect x="4" y="4" width="16" height="16" rx="2" />
-                    <path d="M10 10l4 4m0 -4l-4 4" />
-                    </svg>`
-                    divOption.appendChild(deleteButton);
-                    new CustomSelect().replace(button.parentNode,divOption,button);
-                    document.querySelector('.div-search').remove();
+                    obj.selectOption(button,divOption);
                     divOption.removeEventListener('click',handler);
-                    deleteButton.addEventListener('click',()=>{
-                        new CustomSelect().replace(divOption.parentNode,button,divOption);
-                    },true);
                 })
                 document.querySelector('.content').appendChild(divOption);
             })
+    }
+    search(e){
+        const container = document.querySelector('div.content');
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+        const value = this.deleteDiacritic(e.target.value.toUpperCase());
+        if(value){
+            const newOptions = this.options.filter(option=>this.deleteDiacritic(option.label).includes(value));
+            if(newOptions.length===0){
+                while (container.firstChild) {
+                    container.removeChild(container.firstChild);
+                }
+            }
+            this.createOptions(newOptions,this.button);
+        }else{
+            this.createOptions(this.options,this.button);
+        }
     }
     getOptions(endPoint){
         return new Promise((resolve, reject) =>{
@@ -45,8 +76,6 @@ export default class CustomSelect{
         }) 
     }
     async createWindow(){
-        await this.getOptions(this.endPoint)
-            .then(json => this.options = json);
         const window = UI.createElement('div',{},['div-search']);
         const inputSearch = UI.createElement('div',{},['search']);
         const content = UI.createElement('div',{},['content']);
@@ -68,12 +97,18 @@ export default class CustomSelect{
             placeholder:'Buscar',
             id:'search',
         });
+        input.addEventListener('input',e=>{
+            this.search(e)
+        })
         inputContainer.append(svgSearch,input);
         inputSearch.append(inputContainer,buttonClose);
         window.append(inputSearch,content);
         this.closeWindow(buttonClose);
         document.querySelector('form').appendChild(window);
-        this.selectOption(this.options,this.button);
+        UI.spinner(content);
+        await this.getOptions(this.endPoint)
+            .then(json => this.options = json);
+        this.createOptions(this.options,this.button);
     }
     closeWindow(button){
         button.addEventListener('click',()=>document.querySelector('.div-search').remove());
